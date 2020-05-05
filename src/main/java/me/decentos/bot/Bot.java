4626eb4a8efb5c0c7369b7b5377ebc35aa7fa519
Vlad Matveev <matveev_vlad@me.com>
2020-05-05 20:53:37 +0300

    Create buttons generator

--- src/main/java/me/decentos/bot/Bot.java
+++ src/main/java/me/decentos/bot/Bot.java
@@ -3,19 +3,15 @@ package me.decentos.bot;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 
 import static org.telegram.telegrambots.logging.BotLogger.log;
 
 public class Bot extends TelegramLongPollingBot {
+    private final Button button = new Button();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -25,7 +21,7 @@ public class Bot extends TelegramLongPollingBot {
             if (text.equals("/start")) {
                 sendMessage(update.getMessage().getChatId().toString(), message, "Выберите номер билета");
             }
-            else if (text.matches("/\\d*")) {
+            else if (text.matches("№\\s\\d*")) {
                 sendMessage(update.getMessage().getChatId().toString(), message, "Вы выбрали билет №" + text.substring(1));
             }
             else {
@@ -41,104 +37,15 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setText(text);
         try {
             if (message.getText().equals("/start")) {
-                setTicketButtons(sendMessage);
+                button.setTicketButtons(sendMessage);
             }
-            else if (message.getText().matches("/\\d*")) {
-                setAnswerButtons(sendMessage);
+            else if (message.getText().matches("№\\s\\d*")) {
+                button.setAnswerButtons(sendMessage);
             }
             execute(sendMessage);
         } catch (TelegramApiException e) {
             log(Level.SEVERE, "Exception: ", e.toString());
         }
-    }
-
-    public synchronized void setTicketButtons(SendMessage sendMessage) {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-        List<KeyboardRow> keyboard = new ArrayList<>();
-
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-        keyboardFirstRow.add(new KeyboardButton("/1"));
-        keyboardFirstRow.add(new KeyboardButton("/2"));
-        keyboardFirstRow.add(new KeyboardButton("/3"));
-        keyboardFirstRow.add(new KeyboardButton("/4"));
-        keyboardFirstRow.add(new KeyboardButton("/5"));
-        keyboardFirstRow.add(new KeyboardButton("/6"));
-        keyboardFirstRow.add(new KeyboardButton("/7"));
-        keyboardFirstRow.add(new KeyboardButton("/8"));
-
-        KeyboardRow keyboardSecondRow = new KeyboardRow();
-        keyboardSecondRow.add(new KeyboardButton("/9"));
-        keyboardSecondRow.add(new KeyboardButton("/10"));
-        keyboardSecondRow.add(new KeyboardButton("/11"));
-        keyboardSecondRow.add(new KeyboardButton("/12"));
-        keyboardSecondRow.add(new KeyboardButton("/13"));
-        keyboardSecondRow.add(new KeyboardButton("/14"));
-        keyboardSecondRow.add(new KeyboardButton("/15"));
-        keyboardSecondRow.add(new KeyboardButton("/16"));
-
-        KeyboardRow keyboardThirdRow = new KeyboardRow();
-        keyboardThirdRow.add(new KeyboardButton("/17"));
-        keyboardThirdRow.add(new KeyboardButton("/18"));
-        keyboardThirdRow.add(new KeyboardButton("/19"));
-        keyboardThirdRow.add(new KeyboardButton("/20"));
-        keyboardThirdRow.add(new KeyboardButton("/21"));
-        keyboardThirdRow.add(new KeyboardButton("/22"));
-        keyboardThirdRow.add(new KeyboardButton("/23"));
-        keyboardThirdRow.add(new KeyboardButton("/24"));
-
-        KeyboardRow keyboardFourthRow = new KeyboardRow();
-        keyboardFourthRow.add(new KeyboardButton("/25"));
-        keyboardFourthRow.add(new KeyboardButton("/26"));
-        keyboardFourthRow.add(new KeyboardButton("/27"));
-        keyboardFourthRow.add(new KeyboardButton("/28"));
-        keyboardFourthRow.add(new KeyboardButton("/29"));
-        keyboardFourthRow.add(new KeyboardButton("/30"));
-        keyboardFourthRow.add(new KeyboardButton("/31"));
-        keyboardFourthRow.add(new KeyboardButton("/32"));
-
-        KeyboardRow keyboardFifthRow = new KeyboardRow();
-        keyboardFifthRow.add(new KeyboardButton("/33"));
-        keyboardFifthRow.add(new KeyboardButton("/34"));
-        keyboardFifthRow.add(new KeyboardButton("/35"));
-        keyboardFifthRow.add(new KeyboardButton("/36"));
-        keyboardFifthRow.add(new KeyboardButton("/37"));
-        keyboardFifthRow.add(new KeyboardButton("/38"));
-        keyboardFifthRow.add(new KeyboardButton("/39"));
-        keyboardFifthRow.add(new KeyboardButton("/40"));
-
-        keyboard.add(keyboardFirstRow);
-        keyboard.add(keyboardSecondRow);
-        keyboard.add(keyboardThirdRow);
-        keyboard.add(keyboardFourthRow);
-        keyboard.add(keyboardFifthRow);
-        replyKeyboardMarkup.setKeyboard(keyboard);
-    }
-
-    public synchronized void setAnswerButtons(SendMessage sendMessage) {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-        List<KeyboardRow> keyboard = new ArrayList<>();
-
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-        keyboardFirstRow.add(new KeyboardButton("1"));
-        keyboardFirstRow.add(new KeyboardButton("2"));
-
-        KeyboardRow keyboardSecondRow = new KeyboardRow();
-        keyboardSecondRow.add(new KeyboardButton("3"));
-        keyboardSecondRow.add(new KeyboardButton("4"));
-
-        keyboard.add(keyboardFirstRow);
-        keyboard.add(keyboardSecondRow);
-        replyKeyboardMarkup.setKeyboard(keyboard);
     }
 
     @Override
---
+++ src/main/java/me/decentos/bot/Button.java
@@ -0,0 +1,75 @@
+package me.decentos.bot;
+
+import lombok.val;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Button {
+
+    public synchronized void setTicketButtons(SendMessage sendMessage) {
+        val replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        val keyboard = createKeyboardTemplate(replyKeyboardMarkup, sendMessage);
+
+        val keyboardFirstRow = new KeyboardRow();
+        fillKeyboard(keyboardFirstRow, 1, 8, true);
+
+        val keyboardSecondRow = new KeyboardRow();
+        fillKeyboard(keyboardSecondRow, 9, 8, true);
+
+        val keyboardThirdRow = new KeyboardRow();
+        fillKeyboard(keyboardThirdRow, 17, 8, true);
+
+        val keyboardFourthRow = new KeyboardRow();
+        fillKeyboard(keyboardFourthRow, 25, 8, true);
+
+        val keyboardFifthRow = new KeyboardRow();
+        fillKeyboard(keyboardFifthRow, 33, 8, true);
+
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        keyboard.add(keyboardThirdRow);
+        keyboard.add(keyboardFourthRow);
+        keyboard.add(keyboardFifthRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    public synchronized void setAnswerButtons(SendMessage sendMessage) {
+        val replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        val keyboard = createKeyboardTemplate(replyKeyboardMarkup, sendMessage);
+
+        val keyboardFirstRow = new KeyboardRow();
+        fillKeyboard(keyboardFirstRow, 1, 2, false);
+
+        val keyboardSecondRow = new KeyboardRow();
+        fillKeyboard(keyboardSecondRow, 3, 2, false);
+
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    private List<KeyboardRow> createKeyboardTemplate(ReplyKeyboardMarkup replyKeyboardMarkup, SendMessage sendMessage) {
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        return new ArrayList<>();
+    }
+
+    private void fillKeyboard(KeyboardRow keyboardRow, int initValue, int countButton, boolean isTicket) {
+        for (int i = 0; i < countButton; i++) {
+            if (isTicket) {
+                keyboardRow.add(new KeyboardButton(String.format("№ %d", initValue)));
+            }
+            else {
+                keyboardRow.add(new KeyboardButton(String.valueOf(initValue)));
+            }
+            initValue++;
+        }
+    }
+}
