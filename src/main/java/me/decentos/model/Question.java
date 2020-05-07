38390ad2eb88233d382c78d4f57f23ad84d5f53b
Vlad Matveev <matveev_vlad@me.com>
2020-05-07 13:43:06 +0300

    Create Models

---
+++ src/main/java/me/decentos/model/Question.java
@@ -0,0 +1,32 @@
+package me.decentos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private int id;
+
+    @Column(name = "ticket")
+    private int ticket;
+
+    @Column(name = "question_title")
+    private String questionTitle;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    @Column(name = "comment")
+    private String comment;
+}
