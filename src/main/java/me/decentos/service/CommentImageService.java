cadefc46bfecf57e7342d242830070e510d0b490
Vlad Matveev <matveev_vlad@me.com>
2020-05-22 10:38:43 +0300

    Delete CommentImage

--- src/main/java/me/decentos/service/CommentImageService.java
+++
@@ -1,9 +0,0 @@
-package me.decentos.service;
-
-import me.decentos.model.CommentImage;
-
-import java.util.List;
-
-public interface CommentImageService {
-    List<CommentImage> findCommentImagesByQuestionId(int questionId);
-}
